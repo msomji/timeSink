feee6c8e235bffca353ba57e15c46afd550ad5b4
Maisam Somji <maisam.somji@gmail.com>
2021-01-26 13:23:16 -0500

    add nextJs app

---
+++ time_sink_ui/public/visitHistory.js
@@ -0,0 +1,11 @@
+let awaitDataCollection = async () => {
+  console.log(typeof visitHistory=== undefined)
+  while ((typeof visitHistory) === "undefined") {
+    console.log('waiting')
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  }
+  console.log('recieved visitHistory.');
+  console.log(visitHistory)
+}
+console.log('wer')
+awaitDataCollection()
